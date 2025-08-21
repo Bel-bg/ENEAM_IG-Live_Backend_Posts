176e79aa93af98fc07ee2a1198cdcfb1229749cb
Belmonde FIODEKANHOUN <belmondefio@gmail.com>
2025-08-21 10:16:48 +0100

    ajust imagekit env

--- routes/imagekit.js
+++ routes/imagekit.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
-  publicKey: "PUBLIC_KEY",
-  privatekey: "PRIVATE_KEY",
+  publicKey: process.env.PUBLIC_KEY,
+  privatekey: process.env.PRIVATE_KEY,
   urlEndpoint: "https://ik.imagekit.io/tynqoj7oy/",
 });
 
